f1715970e27ab8f9e14f818f44fddd94d5cdb968
lucassoliz <lsilessoliz@frba.utn.edu.ar>
2026-01-01 18:37:31 -0300

    test(): probando IDE 28

--- pokemon/src/main/java/com/mycompany/pokemon/Pokemon.java
+++ pokemon/src/main/java/com/mycompany/pokemon/Pokemon.java
@@ -4,7 +4,7 @@ package com.mycompany.pokemon;
 public class Pokemon {
 
     public static void main(String[] args) {
-        System.out.println("Probando IDE NetBeans");
+        System.out.println("Probando IDE NetBeans 28");
         System.out.println("Probando si salio todo bien");
     }
 }
