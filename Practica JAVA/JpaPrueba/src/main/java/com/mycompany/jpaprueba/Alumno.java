a3a0996c01a07bc2c71b8976e043e0826f68b8f3
lucassoliz <lsilessoliz@frba.utn.edu.ar>
2026-01-06 18:21:23 -0300

    JPA

---
+++ Practica JAVA/JpaPrueba/src/main/java/com/mycompany/jpaprueba/Alumno.java
@@ -0,0 +1,73 @@
+
+package com.mycompany.jpaprueba;
+
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+ public class Alumno {
+ /*Hasta con la ORM podemos decirle a nuestra BD
+    Che creame este valor y este id generamelo de forma automatica
+    cada vez que recibas un nuevp registro*/  
+//Es importante la ubicacion de los dos @ deben ir juntos y arriba de mid PK
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO) //Sequence para que vaya como una secuencia; 1,2,3... y AUTO para que el mismo elija cual sea la mejor opcion
+     private int id;
+    @Basic   //Aunque no hace falta, JPA ya lo toma como Basic si no le ponemos nada
+     private String nombre;
+     private String apellido;
+     
+     @Temporal(TemporalType.DATE)
+     private Date fechaNac;
+
+    public Alumno() {
+    }
+
+    public Alumno(int id, String nombre, String apellido, Date fechaNac) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNac = fechaNac;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+     
+     
+     
+}
---
+++ Practica JAVA/JpaPrueba/src/main/java/com/mycompany/jpaprueba/JpaPrueba.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.jpaprueba;
+
+public class JpaPrueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
