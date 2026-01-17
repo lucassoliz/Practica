59a0e897883fc615686868b0eb89d616a9be5b08
lucassoliz <lsilessoliz@frba.utn.edu.ar>
2026-01-17 01:28:30 -0300

    feat(): add SvUsuarios struct

---
+++ Practica APP Web/PrimerServlet/src/main/java/com/mycompany/primerservlet/servlets/SvUsuarios.java
@@ -0,0 +1,41 @@
+
+package com.mycompany.primerservlet.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "SvUsuarios", urlPatterns = {"/SvUsuarios"})
+public class SvUsuarios extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //vamos a usar el modelo de capas asi que no vamos a usar nada de aca
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
